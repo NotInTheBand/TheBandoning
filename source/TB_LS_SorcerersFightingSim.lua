1dc1c423495d7c390c8557f9d3dbd2deefc406b2
TheBand <bphos@outlook.com>
2020-12-15 23:55:29 -0600

    Update TB_LS_SorcerersFightingSim.lua

--- source/TB_LS_SorcerersFightingSim.lua
+++ source/TB_LS_SorcerersFightingSim.lua
@@ -1,4 +1,4 @@
 getgenv().Key = "KEY"
-getgenv().Buyer = "BUYER DISCORD"
+getgenv().Buyer = "BUYER USERNAME"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NotInTheBand/TheBandoning/main/obf/TB_SorcerersFightingSim.lua", true))()
