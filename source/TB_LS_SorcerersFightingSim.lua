8ff2ad2614682060f0e70960829d33ca157812a9
TheBand <bphos@outlook.com>
2020-12-21 14:05:31 -0600

    f

--- source/TB_LS_SorcerersFightingSim.lua
+++
@@ -1,4 +0,0 @@
-getgenv().Key = "KEY"
-getgenv().Buyer = "DISCORD USERNAME"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NotInTheBand/TheBandoning/main/obf/TB_SorcerersFightingSim.lua", true))()
